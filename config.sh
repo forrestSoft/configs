f46bf4ca647cf61eb6ffa6ccae69c377014f8897
jeff forrester <jeff.forrester@gmail.com>
2019-04-04 22:09:38 -0400

    more evn config:

--- config.sh
+++ config.sh
@@ -9,4 +9,18 @@ gunzip -c ~/Downloads/ttf-bitstream-vera-1.10.tar.bz2 | tar xpf
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git
+brew install bash-completion
+git config --global alias.co checkout
+git config --global alias.st status
 brew install npm
+
+
+
+
+
+# .bash_profile
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion || {
+    # if not found in /usr/local/etc, try the brew --prefix location
+    [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ] && \
+        . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+}
